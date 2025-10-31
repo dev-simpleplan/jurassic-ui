bb7510f03b11218a1b80c43647402d32791d4d2f
dev-simpleplan <simple.website.testing@gmail.com>
2025-10-31 11:52:17 +0530

    update

---
+++ assets/custom.js
@@ -0,0 +1,19 @@
+// $('.jw-carousel .owl-carousel').owlCarousel({
+//     items:1,
+//     nav: true,
+//     dots: false,
+//     loop:false,
+//     margin:10,
+//     animateOut: 'slideOutUp',
+//     animateIn: 'slideInUp',
+//     URLhashListener:true,
+//     autoplayHoverPause:true,
+//     startPosition: 'URLHash'
+// });
+
+$('.jw-carousel').slick({
+  vertical: true,
+  verticalSwiping: true,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
