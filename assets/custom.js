f706369e3b8d3537b4d89d095a090654d20d0647
dev-simpleplan <95477430+dev-simpleplan@users.noreply.github.com>
2026-04-30 17:52:01 +0530

    update

--- assets/custom.js
+++ assets/custom.js
@@ -209,6 +209,8 @@ window.bindProductCardAddForms = function (scope = document) {
       }
 
       try {
+        await wait(250);
+
         const response = await fetch('/cart/add.js', {
           method: 'POST',
           body: new FormData(form),
@@ -279,33 +281,35 @@ const sidebar = document.querySelector('.filter-sidebar');
 const closeBtn = document.querySelector('.filter-close-mob');
 const body = document.body;
 
-// Function to sync body state
-function syncBodyState() {
-  if (sidebar.classList.contains('active')) {
-    body.setAttribute('data-lenis-prevent', '');
-    body.classList.add('backdrop');
-  } else {
-    body.removeAttribute('data-lenis-prevent');
-    body.classList.remove('backdrop');
+if (sidebar && closeBtn) {
+  // Function to sync body state
+  function syncBodyState() {
+    if (sidebar.classList.contains('active')) {
+      body.setAttribute('data-lenis-prevent', '');
+      body.classList.add('backdrop');
+    } else {
+      body.removeAttribute('data-lenis-prevent');
+      body.classList.remove('backdrop');
+    }
   }
+
+  // Watch for class changes (in case it's toggled elsewhere)
+  const observer = new MutationObserver(syncBodyState);
+
+  observer.observe(sidebar, {
+    attributes: true,
+    attributeFilter: ['class']
+  });
+
+  // Close button click
+  closeBtn.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+    syncBodyState(); // ensure cleanup immediately
+  });
+
+  // Run once on load
+  syncBodyState();
 }
-
-// Watch for class changes (in case it's toggled elsewhere)
-const observer = new MutationObserver(syncBodyState);
-
-observer.observe(sidebar, {
-  attributes: true,
-  attributeFilter: ['class']
-});
-
-// Close button click
-closeBtn.addEventListener('click', () => {
-  sidebar.classList.remove('active');
-  syncBodyState(); // ensure cleanup immediately
-});
-
-// Run once on load
-syncBodyState();
 
 
 document.addEventListener("DOMContentLoaded", function () {
